eae47a0e7bd5c43e998023291c5bfb9665b1cd16
Per Kraulis <per.kraulis@scilifelab.se>
2014-07-07 14:32:32 +0200

    Added command-line handling for init_database.py

--- charon/init_database.py
+++ charon/init_database.py
@@ -1,4 +1,4 @@
-""" Charon: Initialize the database.
+""" Charon: Initialize the database, working directly towards CouchDB.
 
 1) Wipeout the old database.
 2) Load the design documents.
@@ -6,7 +6,6 @@
 """
 
 import os
-import getpass
 
 from charon import constants
 from charon import utils
@@ -22,23 +21,34 @@ def wipeout_database(db):
 
 if __name__ == '__main__':
     import sys
-    response = raw_input('about to delete everything; really sure? [n] > ')
-    try:
-        utils.load_settings(filepath=sys.argv[1])
-    except IndexError:
-        utils.load_settings()
-    if utils.to_bool(response):
-        db = utils.get_db()
-        wipeout_database(db)
-        print 'wiped out database'
-        load_designs(db)
-        print 'loaded designs'
-        default = 'dump.tar.gz'
+    import argparse
+    parser = argparse.ArgumentParser(description='Initialize and load Charon database from dump file')
+    parser.add_argument('filepath', type=str, nargs='?', default=None,
+                        help='filepath for YAML settings file')
+    parser.add_argument('--force', dest='force', action='store_true',
+                        help='force action, rather than ask for confirmation')
+    args = parser.parse_args()
+    
+    if not args.force:
+        response = raw_input('about to delete everything; really sure? [n] > ')
+        if not utils.to_bool(response):
+            sys.exit('aborted')
+    utils.load_settings(filepath=args.filepath)
+
+    db = utils.get_db()
+    wipeout_database(db)
+    print 'wiped out database'
+    load_designs(db)
+    print 'loaded designs'
+    default = 'dump.tar.gz'
+    if args.force:
+        filename = default
+    else:
         filename = raw_input("load data from file? [{0}] > ".format(default))
         if not filename:
             filename = default
-        if os.path.exists(filename):
-            count_items, count_files = undump(db, filename)
-            print 'undumped', count_items, 'items and', count_files, 'files from', filename
-        else:
-            print 'no such file to undump'
+    if os.path.exists(filename):
+        count_items, count_files = undump(db, filename)
+        print 'undumped', count_items, 'items and', count_files, 'files from', filename
+    else:
+        print 'no such file to undump'
